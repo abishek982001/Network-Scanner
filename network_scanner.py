249324e6b679afdf6f5a6de41523c40e47b08118
abishek982001 <abishek982001@gmail.com>
2020-04-02 04:06:28 -0400

    used dictionaries with lists

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP/ IP range")
+    options = parser.parse_args()
+    return options
+
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -8,8 +17,19 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request   # creating a new packet
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]  # send the packet and return the response
 
-    print("IP\t\t\tMAC ADDRESS\n-----------------------------------------")
+    clients_list = []  # creating a client list
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}  # creating a dictionary to store IP and MAC address from client list
+        clients_list.append(client_dict)
+    return clients_list
 
-scan("10.0.2.1/24")
+
+def print_result(results_list):
+    print("IP\t\t\tMAC ADDRESS\n-----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+# main function
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
